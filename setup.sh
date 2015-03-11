aa8e66154ad7537d384d037920b421fcd43944d0
alrighttheresham <damian.oneill@gmail.com>
2015-03-11 17:33:11 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 
 brew doctor; brew update
 
-brew install git vim zsh zsh-completions whatmask htop tmux the_silver_searcher ctags autojump bash-completion ssh-copy-id tiff2png tree watch wget xz 
+brew install git svn vim zsh zsh-completions whatmask htop tmux the_silver_searcher ctags autojump bash-completion ssh-copy-id tiff2png tree watch wget xz 
 
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 
@@ -18,7 +18,7 @@ brew tap caskroom/cask; brew install brew-cask; brew update && brew upgrade brew
 
 brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package
 
-brew cask install dropbox chrome github chicken cyberduck xquartz firefox intellij-idea iterm2 sourcetree virtualbox sublime-text sequel-pro
+brew cask install java dropbox chrome github chicken cyberduck xquartz firefox intellij-idea iterm2 sourcetree virtualbox sublime-text sequel-pro
 
 curl http://j.mp/spf13-vim3 -L -o - | sh
 
